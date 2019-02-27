6379ea26212fa375ba6b25c94444a5dbb58aad21
Jared Harbison <jared.harbison@gmail.com>
2019-02-27 19:06:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,23 +1,29 @@
-require './environment'
+get '/' do
+  erb :root
+end
 
-module FormsLab
-  class App < Sinatra::Base
+ get '/new' do
+  erb :'pirates/new'
+end
 
-    get '/' do
-      erb :root
-    end
+ post '/pirates' do
+  @pirate = Pirate.new(params[:pirate])
 
-    get '/new' do
-      erb :"pirate/new"
-    end
-
-    post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
-      params[:pirate][:ships].each do |ship|
-        Ship.new(ship)
-      end
-      @ships = Ship.all
-      erb :"pirates/show"
-    end
+   params[:pirate][:ships].each do |details|
+    Ship.new(details)
   end
-end 
+
+   @ship = Ship.all
+
+   @ship_name_1 = @ship[0].name
+  @ship_type_1 = @ship[0].type
+  @ship_booty_1 = @ship[0].booty
+  @ship_name_2 = @ship[1].name
+  @ship_type_2 = @ship[1].type
+  @ship_booty_2 = @ship[1].booty
+
+   erb :'pirates/show'
+end
+end
+end
+ 
